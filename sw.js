fa2901e848712c5463207d48c01c5adc9cba7376
guozr <guozr@legion-tech.net>
2019-01-04 17:52:38 +0800

    update

--- sw.js
+++ sw.js
@@ -9,39 +9,43 @@ var cacheList = [
     'appIcon.png'
 ]
 
-self.addEventListener('install',e =>{
-  e.waitUntil(
-    caches.open(cacheStorageKey)
-    .then(cache => cache.addAll(cacheList))
-    .then(() => self.skipWaiting())
-  )
+// self.addEventListener('install', function(e) {
+//   e.waitUntil(
+//     caches.open(cacheStorageKey)
+//     .then(cache => cache.addAll(cacheList))
+//     .then(() => self.skipWaiting())
+//   )
+// })
+
+self.addEventListener('install',function(e) {
+  return self.skipWaiting();
 })
 
-self.addEventListener('fetch',function(e){
-  e.respondWith(
-    caches.match(e.request).then(function(response){
-      if(response != null){
-        return response
-      }
-      return fetch(e.request.url)
-    })
-  )
-})
+// self.addEventListener('fetch',function(e){
+//   e.respondWith(
+//     caches.match(e.request).then(function(response){
+//       if(response != null){
+//         return response
+//       }
+//       return fetch(e.request.url)
+//     })
+//   )
+// })
 
-self.addEventListener('activate',function(e){
-  e.waitUntil(
-    //获取所有cache名称
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        // 获取所有不同于当前版本名称cache下的内容
-        cacheNames.filter(cacheNames => {
-          return cacheNames !== cacheStorageKey
-        }).map(cacheNames => {
-          return caches.delete(cacheNames)
-        })
-      )
-    }).then(() => {
-      return self.clients.claim()
-    })
-  )
-})
+// self.addEventListener('activate',function(e){
+//   e.waitUntil(
+//     //获取所有cache名称
+//     caches.keys().then(cacheNames => {
+//       return Promise.all(
+//         // 获取所有不同于当前版本名称cache下的内容
+//         cacheNames.filter(cacheNames => {
+//           return cacheNames !== cacheStorageKey
+//         }).map(cacheNames => {
+//           return caches.delete(cacheNames)
+//         })
+//       )
+//     }).then(() => {
+//       return self.clients.claim()
+//     })
+//   )
+// })
